a8fbbc259a34919a5f8b4e31d594d0bba6d6abf9
Kevin Musiorski <RudyOnRails@gmail.com>
2021-05-08 22:34:33 -0500

    start a request spec...

---
+++ spec/requests/exam_registrations_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'api/v1/exam_registrations.json' do
+  describe 'POST' do
+
+    let(:college) { College.create }
+    let(:exam)    { college.exams.create(start_time: 3.days.ago, end_time: 1.days.from_now) }
+    let(:params) do
+      {
+        first_name: "Kevin",
+        last_name: "Musiorski",
+        phone_number: "13127725636",
+        college_id: college.id,
+        exam_id: exam.id,
+      }
+    end
+
+    fit "returns 200 with good params" do
+      get('/api/v1/exam_registrations.json', params)
+      expect(response).to have_http_status :ok
+    end
+  end
+end
