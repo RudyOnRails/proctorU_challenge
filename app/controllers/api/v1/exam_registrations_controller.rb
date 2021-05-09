454d7da01e2aaa076ce1d5c9523332873ba968b9
Kevin Musiorski <RudyOnRails@gmail.com>
2021-05-09 09:46:16 -0500

    don't try to use fancy upsert yet if you don't understand it, just use tried and true find_or_create_by.

--- app/controllers/api/v1/exam_registrations_controller.rb
+++ app/controllers/api/v1/exam_registrations_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::ExamRegistrationsController < ApplicationController
   def create
-    user = User.upsert(user_params)
+    user = User.find_or_create_by(first_name: exam_registration_params[:first_name])
   end
 
   private
-    def user_params
-      params.require(:first_name)
+    def exam_registration_params
+      params.require(:exam_registration).tap do |params|
+        params.require(:first_name)
+      end
     end
 end
