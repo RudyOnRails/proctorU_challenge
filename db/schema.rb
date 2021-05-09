552ab01f16dd22727b1021c77fed9f3a7ef68768
Kevin Musiorski <RudyOnRails@gmail.com>
2021-05-09 00:12:35 -0500

    NameError:        uninitialized constant Api::V1::ExamRegistrationsController::User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_041820) do
+ActiveRecord::Schema.define(version: 2021_05_09_051153) do
 
   create_table "colleges", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_05_09_041820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["college_id"], name: "index_exams_on_college_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "exams", "colleges"
