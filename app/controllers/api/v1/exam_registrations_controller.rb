260b0460edf6bce46b9c3452c9abe0a87927b9ca
Kevin Musiorski <RudyOnRails@gmail.com>
2021-05-11 20:34:06 -0500

    rescue and customize strong params error

--- app/controllers/api/v1/exam_registrations_controller.rb
+++ app/controllers/api/v1/exam_registrations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ExamRegistrationsController < ApplicationController
 
-  rescue_from ActionController::ParameterMissing do
-    head :bad_request
+  rescue_from ActionController::ParameterMissing do |e|
+    render json: e.message, status: 400
   end
 
   def create
